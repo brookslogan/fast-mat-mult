24799111b815b0dd615ea5b752df339626d78a04
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2025-07-10 00:21:06 -0700

    WIP debugging some surprises

--- fourier-mat-mult.R
+++ fourier-mat-mult.R
@@ -14,6 +14,8 @@ omegaIJK <- exp(2*pi*1i/(I*J*K))
 
 # i, IjK, Ik
 
+# careless 1-index-based seems to work
+
 # Xhat <- sapply(seq_len(K), function(khat) {
 #   sapply(seq_len(I), function(ihat) {
 #     is <- row(X)
@@ -31,7 +33,7 @@ omegaIJK <- exp(2*pi*1i/(I*J*K))
 # })
 
 # XY <- X %*% Y
-# Zhatpp <- sapply(seq_len(K), function(khat) {
+# XYhatpp <- sapply(seq_len(K), function(khat) {
 #   sapply(seq_len(I), function(ihat) {
 #     is <- row(XY)
 #     ks <- col(XY)
@@ -39,13 +41,47 @@ omegaIJK <- exp(2*pi*1i/(I*J*K))
 #   })
 # })
 
-# (Xhat * Yhatp / Zhatpp) %>% {. / .[[1]]} %>% {. / ((4:18)/4)}
+# (Xhat * Yhatp / XYhatpp) %>% {. / .[[1]]} %>% {. / ((4:18)/4)}
+
+# 0-index-based
+
+# Xhat <- sapply(seq_len(K) - 1L, function(khat) {
+#   sapply(seq_len(I) - 1L, function(ihat) {
+#     is <- row(X) - 1L
+#     js <- col(X) - 1L
+#     sum(X * omegaIJK^((is + I*js*K))*(ihat + I*khat))
+#   })
+# })
+
+# Yhatp <- sapply(seq_len(K) - 1L, function(khat) {
+#   sapply(seq_len(I) - 1L, function(ihat) {
+#     js <- row(Y) - 1L
+#     ks <- col(Y) - 1L
+#     sum(Y * omegaIJK^(-(I*js*K + I*ks))*(ihat + I*khat))
+#   })
+# })
+
+# XY <- X %*% Y
+# XYhatpp <- sapply(seq_len(K) - 1L, function(khat) {
+#   sapply(seq_len(I) - 1L, function(ihat) {
+#     is <- row(XY) - 1L
+#     ks <- col(XY) - 1L
+#     sum(XY * omegaIJK^((is + I*ks))*(ihat + I*khat))
+#   })
+# })
+
+# stopifnot(dplyr::near((Xhat * Yhatp)[[1L]], XYhatpp[[1L]]))
+# stopifnot(all(dplyr::near(((Xhat * Yhatp) / XYhatpp) %>% {. / .[[2L]]} %>% {. / (seq_along(.) - 1L)} %>% {.[-1]}, 1)))
+
+# NOTE for DFT, omega exponents above probably need changed to be negative
+
+# 0-index-based, matching DFT power signs
 
 Xhat <- sapply(seq_len(K) - 1L, function(khat) {
   sapply(seq_len(I) - 1L, function(ihat) {
     is <- row(X) - 1L
     js <- col(X) - 1L
-    sum(X * omegaIJK^((is + I*js*K))*(ihat + I*khat))
+    sum(X * omegaIJK^(-(is + I*js*K))*(ihat + I*khat))
   })
 })
 
@@ -53,17 +89,82 @@ Yhatp <- sapply(seq_len(K) - 1L, function(khat) {
   sapply(seq_len(I) - 1L, function(ihat) {
     js <- row(Y) - 1L
     ks <- col(Y) - 1L
-    sum(Y * omegaIJK^(-(I*js*K + I*ks))*(ihat + I*khat))
+    sum(Y * omegaIJK^((I*js*K + I*ks))*(ihat + I*khat))
   })
 })
 
 XY <- X %*% Y
-Zhatpp <- sapply(seq_len(K) - 1L, function(khat) {
+XYhatpp <- sapply(seq_len(K) - 1L, function(khat) {
   sapply(seq_len(I) - 1L, function(ihat) {
     is <- row(XY) - 1L
     ks <- col(XY) - 1L
-    sum(XY * omegaIJK^((is + I*ks))*(ihat + I*khat))
+    sum(XY * omegaIJK^(-(is + I*ks))*(ihat + I*khat))
   })
 })
 
-((Xhat * Yhatp) / Zhatpp) %>% {. / .[[2L]]} %>% {. / 0:14}
+stopifnot(dplyr::near((Xhat * Yhatp)[[1L]], XYhatpp[[1L]]))
+stopifnot(all(dplyr::near(((Xhat * Yhatp) / XYhatpp) %>% {. / .[[2L]]} %>% {. / (seq_along(.) - 1L)} %>% {.[-1]}, 1)))
+
+# TODO check if Y time reversal is done correctly
+
+# TODO explain the fixups needed
+
+# TODO try to figure out ways to derive hat matrices more quickly
+
+# TODO figure out reverse transform
+
+Xspread <-
+  array(0, c(I, K, J)) %>%
+  {.[,1L,] <- X; dim(.) <- NULL; .}
+
+Yspread <-
+  array(0, c(I, K, J)) %>%
+  {.[1L,,] <- t(Y); dim(.) <- NULL; .}
+
+XYspread <-
+  array(0, c(I, K, J)) %>%
+  {.[,,1L] <- XY; dim(.) <- NULL; .}
+
+omegaIK <- exp(2*pi*1i/(I*K))
+stopifnot(dplyr::near(omegaIK, omegaIJK^J))
+
+XYhatqq <- fft(XYspread)
+
+XYhatqq[J*(row(XY) - 1L + I*(col(XY) - 1L)) + 1L]
+XYhatpp
+# FIXME wrong
+
+
+
+(Xspread * Yspread) %>%
+  # {dim(.) <- c(I*K, J); rowSums(.)} %>%
+  sum() %>%
+  {}
+XY[[1L]]
+
+(Xspread * Yspread[((seq_along(Yspread) - 1L) - 2L) %% length(Yspread) + 1L]) %>%
+  sum() %>%
+  {}
+XY[[1L + 2L]]
+
+dplyr::near(XY,
+            sapply(seq_along(XY) - 1L, function(shift) {
+              (Xspread * Yspread[((seq_along(Yspread) - 1L) - shift) %% length(Yspread) + 1L]) %>%
+                sum() %>%
+                {}
+            }))
+# FIXME wrong
+XY
+sapply(seq_along(XY) - 1L, function(shift) {
+  (Xspread * Yspread[((seq_along(Yspread) - 1L) - shift) %% length(Yspread) + 1L]) %>%
+    sum() %>%
+    {}
+}) %>%
+  `dim<-`(dim(XY))
+
+cbind(Xspread, Yspread)
+
+# This may be connected to initial intuition in notebook sketch that Y->Yhatp
+# transformation should only negate part of exponent.
+
+# TODO finish debugging
