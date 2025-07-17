35ff06311d45d5d7a9befa20b1eb781db74617a5
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2025-07-16 21:46:59 -0700

    Note a few potential tactics to try for dot product transform

--- log-dot-product.R
+++ log-dot-product.R
@@ -4,12 +4,30 @@
 # log-time result with other-vector-independent linear/friendly
 # transformation?
 
-   b1 b2 b3 b4 b5 b6 b7 b8
-a1  1  C  B  B  A  A  A  A
-a2  C  1  B  B  A  A  A  A
-a3  B  B  1  C  A  A  A  A
-a4  B  B  C  1  A  A  A  A
-a5  A  A  A  A  1  C  B  B
-a6  A  A  A  A  C  1  B  B 
-a7  A  A  A  A  B  B  1  C
-a8  A  A  A  A  B  B  C  1
+#    b1 b2 b3 b4 b5 b6 b7 b8
+# a1  1  C  B  B  A  A  A  A
+# a2  C  1  B  B  A  A  A  A
+# a3  B  B  1  C  A  A  A  A
+# a4  B  B  C  1  A  A  A  A
+# a5  A  A  A  A  1  C  B  B
+# a6  A  A  A  A  C  1  B  B
+# a7  A  A  A  A  B  B  1  C
+# a8  A  A  A  A  B  B  C  1
+
+# computing C sums outlined here seems potentially unfriendly.
+
+# Something with parity?
+
+# Some sort of inclusion-exclusion-inclusion-exclusion-etc. approach?
+
+# Rectangles + inclusion-exclusion?
+
+
+# Thought there was something about this sort of pattern extended
+as.matrix(c(1,-1,1,-1)) %*% t(as.matrix(c(1,-1,1,-1))) + as.matrix(c(1,1,-1,-1)) %*% t(as.matrix(c(1,1,-1,-1)))
+# though maybe with alternating +s and -s
+
+
+# parity... something about subtracting away where index bits
+# mismatch?  or getting bits representing the number of bit mismatches
+# in the indices?
